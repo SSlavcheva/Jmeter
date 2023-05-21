97bb6faa56e17b821368ab2f0402c69b65939a2c
Silviya Slavcheva <silviya.slavcheva@gmail.com>
2023-05-21 23:11:55 +0300

    correct script

--- tests_api_perf_tests.ps1
+++ tests_api_perf_tests.ps1
@@ -1,7 +1,4 @@
 
-
-
-
 & "C:\Users\Sylvia\Documents\jmeter2\jmeterproject\apache-jmeter-5.5\bin\jmeter.bat" -n -t "C:\Users\Sylvia\Documents\jmeter2\jmeterproject\apache-jmeter-5.5\bin\test1.jmx"
 Write-Output "##teamcity[blockOpened name='script is run']"
 
